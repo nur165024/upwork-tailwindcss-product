fef581f16f88488e27acd322dacd3b11071f2108
nur165024 <nur42658@gmail.com>
2022-02-04 03:41:36 +0700

    home page design done

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        "secondary-dark-blue": "#263765",
+        "primary-body": "#394A79",
+        "primary-blue": "#2E51FF",
+        "counter-bg": "#263765",
+        "counter-color": "#63E299",
+        "button-color": "#FF7300",
+      },
+    },
+  },
+  plugins: [],
+};
++333333;
